4c5e7db4e6631cfb12135be7ab2ef72e3fc41264
khc9812121 <khc9812121@gmail.com>
2023-11-29 02:16:14 +0900

    [feat] header 반응형 CSS 구현

--- front/src/components/common/TheHeadingView.vue
+++ front/src/components/common/TheHeadingView.vue
@@ -1,49 +1,92 @@
 <script setup>
+import { ref } from "vue";
+const toggle = ref({ hide: true });
+
+const toggleEvent = () => {
+  toggle.value.hide = !toggle.value.hide;
+};
 </script>
 
 <template>
-    <header>
-        <div class="logo">
-            SHOP
+  <header>
+    <div class="layout">
+      <div class="logo">SHOP</div>
+      <nav>
+        <div>Shoes</div>
+        <div>Notice</div>
+        <div>SignIn</div>
+        <div>SignUp</div>
+      </nav>
+      <div @click="toggleEvent" class="dropdown">
+        menu
+        <div :class="toggle" class="menu">
+          <div>Shoes</div>
+          <div>Notice</div>
+          <div>SignIn</div>
+          <div>SignUp</div>
         </div>
-        <div class="layout">
-            <nav>
-                <div>Shoes</div>
-                <div>Notice</div>
-                <div>SignIn</div>
-                <div>SignUp</div>
-            </nav>
-            <div></div>
-        </div>
-    </header>
+      </div>
+      <div class="sup"></div>
+    </div>
+  </header>
 </template>
 
 <style scoped>
-header{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    height: 80px;
-    width: 100%;
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 80px;
+  width: 100%;
 }
-.layout{
-    width: 92%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+.layout {
+  width: 92%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
 }
-.logo{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: fit-content;
+.logo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: fit-content;
 }
-nav{
-    width: 80%;
-    gap: 15px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+nav {
+  width: 80%;
+  gap: 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+.dropdown {
+  display: none;
+  position: relative;
+}
+.hide {
+  display: none;
+}
+.menu {
+  border-radius: 15px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
+    rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+  position: absolute;
+  top: 2.5rem;
+}
+@media (max-width: 896px) {
+  .layout {
+    justify-content: space-between;
+    padding-left: 25px;
+    padding-right: 25px;
+  }
+  nav {
+    display: none;
+  }
+  .dropdown {
+    display: block;
+  }
+  .sup {
+    display: none;
+  }
 }
 </style>
