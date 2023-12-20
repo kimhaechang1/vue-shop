fc36b88865b3de011c39d099ce6ca3cec47af28b
khc9812121 <khc9812121@gmail.com>
2023-12-20 20:10:43 +0900

    [fix] productName 뿐 아니라 product 내 name관련 중복 Exception으로 변경

--- backend/src/main/java/com/khc/shop/product/exception/DuplicatedNameException.java
+++ backend/src/main/java/com/khc/shop/product/exception/DuplicatedNameException.java
@@ -1,10 +1,10 @@
 package com.khc.shop.product.exception;
 
-public class DuplicatedProductNameException extends Exception{
+public class DuplicatedNameException extends Exception{
 
     private final String status = "501";
-    public DuplicatedProductNameException(String productName) {
-        super("제품 명 : "+productName+" 은 이미 존재합니다. !");
+    public DuplicatedNameException(String name) {
+        super("name : "+name+" 은 이미 존재합니다. !");
     }
 
     @Override
---
+++ backend/src/main/java/com/khc/shop/product/model/ProductBrandDto.java
@@ -0,0 +1,2 @@
+package com.khc.shop.product.model;public class ProductBrandDto {
+}
